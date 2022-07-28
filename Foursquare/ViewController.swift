f745e0a1515e0a1f072b13b05e58e320da48c45e
Enes Kaya <eng.eneskaya@gmail.com>
2022-07-28 23:01:30 +0300

    Foursquare Label

--- Foursquare/ViewController.swift
+++ Foursquare/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
