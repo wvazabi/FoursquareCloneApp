dbf94a2359ccdaf8a5c753c496c18c84957aae3c
Enes Kaya <eng.eneskaya@gmail.com>
2022-07-31 19:42:25 +0300

    parse

--- Foursquare/AppDelegate.swift
+++ Foursquare/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+     
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "cBkr8mGUWgdvi7o8YnEEP0epCj5WNU4AWU8iFLIq"
+            $0.clientKey = "uzbKLzn9pMB6FLmlomuDhbNyFwQwSq4Eosf1QVdx"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        let installation = PFInstallation.current()
+        installation!.saveInBackground()
+
+       
         return true
     }
 
--- Foursquare/ViewController.swift
+++ Foursquare/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 class ViewController: UIViewController {
 
@@ -14,7 +16,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+     /*
+        let parseObject = PFObject(className: "Fruit")
+        parseObject["Name"] = "Banana"
+        parseObject["Calories"] = 150
+        parseObject.saveInBackground { success, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            }else{
+                print("uploaded")
+            }
+            
+        }
+        
+        */
+        
+        let query = PFQuery(className: "Fruit")
+        
+        //query.whereKey("Name", contains: "Apple")
+        query.whereKey("Calories", lessThan: 130)
+        query.findObjectsInBackground { objects, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            else{
+                print(objects)
+            }
+        }
+    
+        
+        
+        
     }
+    
+    
     
 
 
